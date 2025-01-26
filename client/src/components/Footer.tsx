6a7479a31c4744e5dd518c819767226d27708644
kobinaq <125668528+kobinaq@users.noreply.github.com>
2025-01-26 20:23:59 +0000

    Add Footer component to the application

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+export function Footer() {
+  return (
+    <footer className="fixed bottom-0 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-t">
+      <div className="container flex h-14 items-center justify-between">
+      </div>
+    </footer>
+  )
+}
